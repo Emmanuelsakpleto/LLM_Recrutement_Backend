1e479ac6e5cd2dc800b244437f04e0a1eb7e788d
Emmanuel SAKPLETO <emmanuelsakpleto1@gmail.com>
2025-06-09 21:52:48 +0000

    #fix

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.String(512))  # Augmenté à 512 caractères
     role = db.Column(db.String(20), default='user')  # 'user' ou 'admin'
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     last_login = db.Column(db.DateTime)
