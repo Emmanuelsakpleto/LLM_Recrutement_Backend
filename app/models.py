6d1a964d06ea506ae1332ff953805814d778f23b
Emmanuel SAKPLETO <emmanuelsakpleto1@gmail.com>
2025-06-12 02:00:37 +0000

    probleme avec user réglé

--- app/models.py
+++ app/models.py
@@ -54,17 +54,10 @@ class Appreciation(db.Model):
     candidate = db.relationship('Candidate', backref=db.backref('appreciations', lazy=True))
 
 class User(db.Model):
-    __tablename__ = 'user'
-    
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(255), nullable=False)  # Utiliser 'password' au lieu de 'password_hash'
-    role = db.Column(db.String(50), default='user')
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    last_login = db.Column(db.DateTime)
-    is_active = db.Column(db.Boolean, default=True)
-    failed_login_attempts = db.Column(db.Integer, default=0)
+    password = db.Column(db.String(200), nullable=False)
 
     def __repr__(self):
         return f'<User {self.username}>'
